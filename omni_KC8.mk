c07abacee252eace8fa2eee02aa441e04d272236
infinixs5lite <kelvinchinedu97@gmail.com>
2023-02-25 21:44:29 +0100

    Update and rename omni_TECNO-KC8.mk to omni_KC8.mk

--- omni_KC8.mk
+++ omni_KC8.mk
@@ -11,13 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from TECNO-KC8 device
-$(call inherit-product, device/tecno/TECNO-KC8/device.mk)
+# Inherit from KC8 device
+$(call inherit-product, device/tecno/KC8/device.mk)
 
-PRODUCT_DEVICE := TECNO-KC8
-PRODUCT_NAME := omni_TECNO-KC8
+PRODUCT_DEVICE := KC8
+PRODUCT_NAME := omni_KC8
 PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO KC8
+PRODUCT_MODEL := TECNO Spark 4
 PRODUCT_MANUFACTURER := tecno
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion-tecno-rev1
